a5a277c44cc72d0b4148d0d1ba2cb381243977f3
sihle1996 <mofokengsihle63@gmail.com>
2024-11-22 04:18:57 -0800

    first commit

---
+++ backend/sporttrials/src/main/java/com/example/sporttrials/SporttrialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.sporttrials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporttrialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporttrialsApplication.class, args);
+	}
+
+}
---
+++ backend/sporttrials/src/test/java/com/example/sporttrials/SporttrialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sporttrials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporttrialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
